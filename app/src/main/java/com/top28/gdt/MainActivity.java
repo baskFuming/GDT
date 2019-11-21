8be42292e3e30f82178d3f9d13cbde9f636496c8
fuming <2394030975@qq.com>
2019-11-21 21:40:24 +0800

    last

---
+++ app/src/main/java/com/top28/gdt/MainActivity.java
@@ -0,0 +1,13 @@
+package com.top28.gdt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
